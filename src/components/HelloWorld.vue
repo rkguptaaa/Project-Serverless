83dc4295c595888abab074609f10d2559e70fb9b
R. K. Gupta <rkguptaaa@gmail.com>
2023-07-12 00:26:25 +0530

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'https://rs5ely6r48.execute-api.us-east-1.amazonaws.com/default/saveobjInto370'
+const API_ENDPOINT = 'https://vhnxv967a3.execute-api.us-east-1.amazonaws.com/default/myFun'
 
 export default {
   name: 'app',
